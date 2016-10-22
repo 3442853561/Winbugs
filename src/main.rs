1206ac853df3b52fa82e04326bddb8260ecddded
刘祺 <3442853561@qq.com>
2016-10-22 14:54:50 +0800

    16.10.22.14.54

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-#[link(name="winbugs")]
+extern crate libc;
+use libc::size_t;
 
+#[link(name="winbugs")]
 extern "C" {
     fn wbMsgbox(text: *const std::os::raw::c_uchar, title: *const ::std::os::raw::c_uchar);
+	fn screenWidth()->size_t;
+	fn screenHeight()->size_t;
 }
 
 fn main() {
@@ -10,4 +14,8 @@ fn main() {
     unsafe {
         wbMsgbox(title.as_ptr(),text.as_ptr());
     }
+	let a: size_t = unsafe {
+		screenWidth()
+	};
+	print!("{}",a);
 }
--- src/winbugs.c
+++ src/winbugs.c
@@ -4,3 +4,11 @@ void wbMsgbox(char* text,char* title)
 {
 	MessageBox (NULL,TEXT(text),TEXT(title),0);
 }
+int screenWidth()
+{
+	return GetSystemMetrics(SM_CXSCREEN);
+}
+int screenHeight()
+{
+	return GetSystemMetrics(SM_CYSCREEN);
+}
