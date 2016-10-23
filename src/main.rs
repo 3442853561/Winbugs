ae8fae75c2879c4f111c731cc2eec3bd105f1ae4
刘祺 <3442853561@qq.com>
2016-10-23 16:43:32 +0800

    16.10.23.16.43

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 extern crate libc;
 use libc::size_t;
+use std::ffi::CString;
+use std::os::raw::c_char;
 
 #[link(name="winbugs")]
 extern "C" {
-    fn wbMsgbox(text: *const std::os::raw::c_uchar, title: *const ::std::os::raw::c_uchar);
+    fn wbMsgbox(text:  *const c_char, title:  *const c_char);
 	fn screenWidth()->size_t;
 	fn screenHeight()->size_t;
+
+}
+
+fn txt(text: &str)->CString{
+	CString::new(text).unwrap()
 }
 
 fn main() {
-    let title = "my_title".to_string();
-    let text = "my_text".to_string();
-    unsafe {
-        wbMsgbox(title.as_ptr(),text.as_ptr());
-    }
-	let a: size_t = unsafe {
+    let title = txt("my_title");
+    let text = txt("my_text");
+	let width: size_t = unsafe {
 		screenWidth()
 	};
-	print!("{}",a);
+	print!("{}",width);
+	let height: size_t = unsafe {
+		screenHeight()
+	};
+	print!("{}",height);
+	unsafe {
+        wbMsgbox(text.as_ptr(),title.as_ptr());
+    }
 }
--- src/winbugs.c
+++ src/winbugs.c
@@ -1,13 +1,17 @@
 //winbugs.c
+#include <stdlib.h>
 #include <windows.h>
+
 void wbMsgbox(char* text,char* title)
 {
-	MessageBox (NULL,TEXT(text),TEXT(title),0);
+	MessageBox (NULL,text,title,0);
 }
+
 int screenWidth()
 {
 	return GetSystemMetrics(SM_CXSCREEN);
 }
+
 int screenHeight()
 {
 	return GetSystemMetrics(SM_CYSCREEN);
